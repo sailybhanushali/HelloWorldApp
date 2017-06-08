cf40f3f9f7b87d01cdb0792168b11f8348b068f1
shadman kudchikar <shadmanssk@yahoo.in>
2017-06-08 17:36:17 +0530

    add contacts list in contacts tab

--- HelloWorld/www/js/my-app.js
+++ HelloWorld/www/js/my-app.js
@@ -1,5 +1,6 @@
 // Initialize your app
 var myApp = new Framework7({
+    precompileTemplates: true,
     pushState: true,
     material: true});
 
@@ -10,5 +11,28 @@ var $$ = Dom7;
 var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
+});
+
+
+
+
+$$('#tab2').on('tab:show', function () {
+    var personHTML = Template7.templates.personTemplate({
+        group:"A",
+        people: [
+            {
+                firstName: 'John',
+                lastName: 'Doe',
+                img: 'http://lorempixel.com/200/200/'
+            },
+            {
+                firstName: 'Mark',
+                lastName: 'Johnson',
+                img: 'http://lorempixel.com/200/200/'
+            }
+        ]
+    });
+
+    $$("#tab2-content").html(personHTML);
 });
 
